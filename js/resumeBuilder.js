0d1daed6578f8bab343268a82cd7373d1e9a7d20
Shawn Clover <git@shawnclover.com>
2014-12-27 11:32:14 -0800

    Format header with name and title

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,6 @@
-$("#main").append(["Shawn Clover"]);
+var name = "Shawn Clover";
+var formattedName = HTMLheaderName.replace ("%data%", name);
+var role = "Web Developer";
+var formattedRole = HTMLheaderRole.replace ("%data%", role);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
